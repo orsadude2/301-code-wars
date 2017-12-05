2623d704ce1352dccaaa290215c3ff5d168ede6e
orsadude2 <dtcomer@gmail.com>
2017-12-04 17:50:45 -0800

    initial submission not finished yet

--- two-word-name/solution.js
+++ two-word-name/solution.js
@@ -2,6 +2,6 @@ var abbrevName = ['Sam Harris', 'Patrick Feenan', 'Evan Cole', 'P Favuzzi', 'Dav
 var inititals = ['S.H', 'P.F', 'E.C', 'P.V', 'D.M'];
 function abbrevName(name){
 for(i = 0; i < abbrevName.length; i++) {
-return(abbrevName[i].charAt(1) + '.' +
+return(abbrevName[i].charAt(1) + '.' + charAt()
 
 }
