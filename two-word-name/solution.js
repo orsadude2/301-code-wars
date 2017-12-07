4041c5ad2d72868d31ffd389b9a984535dcba1e8
orsadude2 <dtcomer@gmail.com>
2017-12-07 13:42:25 -0800

    finished kata 3

--- two-word-name/solution.js
+++ two-word-name/solution.js
@@ -1,7 +1,6 @@
-var abbrevName = ['Sam Harris', 'Patrick Feenan', 'Evan Cole', 'P Favuzzi', 'David Mendiata'];
-var inititals = ['S.H', 'P.F', 'E.C', 'P.V', 'D.M'];
-function abbrevName(name){
-for(i = 0; i < abbrevName.length; i++) {
-return(abbrevName[i].charAt(1) + '.' + charAt()
 
-}
+function abbrevName(name){
+    var matches = name.match(/\b(\w)/g);
+    var acronym = matches[0].toUpperCase() + '.' + matches[1].toUpperCase();
+   return acronym;
+  };
